de9d387c5d1420ff34efb5dae551fa3b3e6cb854
initdc <initd@outlook.com>
2021-12-06 11:32:20 +0800

    [dev.12.6] learn the rest of components

--- pages/test/test.js
+++ pages/test/test.js
@@ -164,6 +164,22 @@ Page({
           songLength: Math.floor(this.innerAudioContext.duration),
         });
       }, 0);
+
+      const query = wx.createSelectorQuery();
+      query
+        .select("#myCanvas")
+        .fields({ node: true, size: true })
+        .exec((res) => {
+          const canvas = res[0].node;
+          const ctx = canvas.getContext("2d");
+
+          const dpr = wx.getSystemInfoSync().pixelRatio;
+          canvas.width = res[0].width * dpr;
+          canvas.height = res[0].height * dpr;
+          ctx.scale(dpr, dpr);
+          ctx.fillStyle = "green";
+          ctx.fillRect(0, 0, 10, 10);
+        });
     });
 
     this.innerAudioContext.onPlay(() => {
